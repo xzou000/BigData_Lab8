3d8894bd23eb5169eb868feb4f7566343ff7003e
xzou000 <xzou000@citymail.cuny.edu>
2018-04-19 16:16:23 -0400

    add py file

---
+++ Lab8.py
@@ -0,0 +1,23 @@
+import pyspark from SparkContext
+
+	
+def extractREST(index, lines):
+    import csv
+    if index==0:
+        lines.next()
+    reader = csv.reader(lines)
+    for row in reader:
+        cuisin = row[7]
+        yield (cuisin, 1)
+		
+def main(sc):
+	NYU_rest='nyc_restaurants.csv'
+	rest = sc.textFile(NYU_rest, use_unicode=False).cache()	
+	rest_cuisin = rest.mapPartitionsWithIndex(extractREST)
+	result=rest_cuisin.reduceByKey(lambda x,y: x+y).sortBy(lambda x: -x[1])
+	return result
+	
+if __name__ == "__main__":
+	sc = SparkContext()
+	# Execute Main functionality
+	main(sc)
